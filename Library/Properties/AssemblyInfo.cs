fda9d6b8eee260017cc20f3aaa923d02172739ae
TomCrypto <thomas.beneteau@yahoo.fr>
2014-11-18 16:52:51 +1300

    Added AssemblyInfo.cs and updated .gitignore.

---
+++ Library/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AntTweakBar.NET")]
+[assembly: AssemblyDescription("C# Wrapper for the AntTweakBar GUI library.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("TomCrypto")]
+[assembly: AssemblyProduct("AntTweakBar.NET")]
+[assembly: AssemblyCopyright("Copyright © 2014-2015 TomCrypto")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b76d7579-202c-41ab-a31e-097cfe9d3f05")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.3.8.0")]
+[assembly: AssemblyFileVersion("0.3.8.0")]
