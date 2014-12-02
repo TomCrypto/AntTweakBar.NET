TomCrypto/AntTweakBar.NET

commit 3f8df7a5373caac0816f6bb21ad5fd64af677ab9

Updated Sample and addressed #3.

Sample/Shader.cs
@@ -16,12 +16,12 @@ namespace Sample
         {
             var str = new StringBuilder();
 
-            str.AppendLine("#version 130");
+            str.AppendLine("#version 120");
             str.AppendLine();
             str.AppendLine("/* DO NOT EDIT - AUTOGENERATED */");
             str.AppendLine();
-            str.AppendLine("in vec3 vertexPosition;");
-            str.AppendLine("out vec2 uv;");
+            str.AppendLine("attribute vec3 vertexPosition;");
+            str.AppendLine("varying vec2 uv;");
             str.AppendLine();
             str.AppendLine("void main(void)");
             str.AppendLine("{");
@@ -41,7 +41,7 @@ namespace Sample
         {
             var shader = String.Join("\n", new[]
             {
-                "#version 130\n",
+                "#version 120\n",
                 "/* DO NOT EDIT - AUTOGENERATED */\n",
                 FragArithmetic(),
                 FragPolyRoots(poly, "poly", hardcodePoly),
@@ -59,7 +59,7 @@ namespace Sample
 
         private static void WriteToFile(String path, String shader)
         {
-            var stream = new StreamWriter(File.OpenWrite(path));
+            var stream = new StreamWriter(File.Open(path, FileMode.Create));
             stream.Write(shader);
             stream.Close();
         }
@@ -142,13 +142,14 @@ namespace Sample
 
             str.AppendLine("uniform vec2 aCoeff;");
             str.AppendLine("uniform vec2 kCoeff;");
-            str.AppendLine("uniform uint iters;");
+            str.AppendLine("uniform int iters;");
             str.AppendLine();
-            str.AppendLine("vec2 iterate(vec2 z, out float speed, out uint t)");
+            str.AppendLine("vec4 iterate(vec2 z)");
             str.AppendLine("{");
-            str.AppendLine("    speed = 0;");
+            str.AppendLine("    float speed;");
+            str.AppendLine("    int t;");
             str.AppendLine();
-            str.AppendLine("    for (t = uint(0); t < iters; ++t)");
+            str.AppendLine("    for (t = int(0); t < iters; ++t)");
             str.AppendLine("    {");
             str.AppendLine("        vec2 r = z;");
             str.AppendLine("        z -= cmul(cdiv(poly(z), derv(z)), aCoeff) + kCoeff;");
@@ -157,7 +158,7 @@ namespace Sample
             str.AppendLine("        if (l < 1e-8) break;");
             str.AppendLine("    }");
             str.AppendLine();
-            str.AppendLine("    return z;");
+            str.AppendLine("    return vec4(z, speed, float(t));");
             str.AppendLine("}");
 
             return str.ToString();
@@ -175,14 +176,12 @@ namespace Sample
             switch (type)
             {
                 case ShadingType.Standard:
-                    str.AppendLine("    if (isnan(speed)) return vec3(1);");
                     str.AppendLine("    speed *= speed * 0.05;");
                     str.AppendLine("    vec3 retval = (sin(vec3(r.x) * palette.xyz) + sin(vec3(r.y) * palette.xyz) + 2) * speed;");
                     str.AppendLine("    return retval / (retval + vec3(1));");
                     break;
                 case ShadingType.Negative:
                     str.AppendLine("    if (speed == 0) return vec3(1);");
-                    str.AppendLine("    if (isnan(speed)) return vec3(0);");
                     str.AppendLine("    vec3 retval = (sin(vec3(r.x) * palette.xyz) + sin(vec3(r.y) * palette.xyz) + 2) / speed;");
                     str.AppendLine("    return retval / (retval + vec3(1));");
                     break;
@@ -207,10 +206,8 @@ namespace Sample
             str.AppendLine();
             str.AppendLine("vec3 shade(vec2 z)");
             str.AppendLine("{");
-            str.AppendLine("    uint t;");
-            str.AppendLine("    float speed;");
-            str.AppendLine("    vec2 r = iterate(z, speed, t);");
-            str.AppendLine("    return colorize(z, r, pow(speed, intensity), t * intensity / iters);");
+            str.AppendLine("    vec4 r = iterate(z);");
+            str.AppendLine("    return colorize(z, r.xy, pow(r.z, intensity), r.w * r.z / iters);");
             str.AppendLine("}");
 
             return str.ToString();
@@ -223,7 +220,7 @@ namespace Sample
             str.AppendLine("uniform vec2 offset;");
             str.AppendLine("uniform float zoom;");
             str.AppendLine("uniform vec2 dims;");
-            str.AppendLine("in vec2 uv;");
+            str.AppendLine("varying vec2 uv;");
             str.AppendLine();
             str.AppendLine("vec3 plot_fractal(vec2 z)");
             str.AppendLine("{");

Library/Context.cs
@@ -23,6 +23,12 @@ namespace AntTweakBar
         private readonly Int32 identifier;
 
         /// <summary>
+        /// Gets this context's last set window size. This is used for release/reset.
+        /// </summary>
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private Size size;
+
+        /// <summary>
         /// Checks whether a graphics API requires a device pointer.
         /// </summary>
         private static bool RequiresDevicePointer(Tw.GraphicsAPI api)
@@ -67,6 +73,25 @@ namespace AntTweakBar
             Tw.Draw(); // Applies to all bars
         }
 
+        /// <summary>
+        /// Releases all graphics resources used by this context. They can be reset with <see cref="AntTweakBar.Context.ResetResources"/>.
+        /// </summary>
+        /// <remarks>
+        /// This does not affect the bars and variables in this context in any way.
+        /// </remarks>
+        public void ReleaseResources()
+        {
+            HandleResize(Size.Empty);
+        }
+
+        /// <summary>
+        /// Resets all graphics resources used by this context.
+        /// </summary>
+        public void ResetResources()
+        {
+            HandleResize(size);
+        }
+
         #region Event Handlers
 
         /// <summary>
@@ -77,6 +102,10 @@ namespace AntTweakBar
         {
             Tw.SetCurrentWindow(Identifier);
             Tw.WindowSize(size.Width, size.Height);
+
+            if (size != Size.Empty) {
+                this.size = size;
+            }
         }
 
         /// <summary>
@@ -232,6 +261,8 @@ namespace AntTweakBar
                 while (disposing && bars.Any()) {
                     bars.First().Dispose();
                 }
+
+                ReleaseResources();
 
                 lock (lk)
                 {

Library/Native.cs
@@ -348,9 +348,9 @@ namespace AntTweakBar
         /// <param name="height">Height of the graphics window.</param>
         public static void WindowSize(int width, int height)
         {
-            if (width <= 0) {
+            if ((width == 0) && (height > 0)) {
                 throw new ArgumentOutOfRangeException("width");
-            } else if (height <= 0) {
+            } else if ((height == 0) && (width > 0)) {
                 throw new ArgumentOutOfRangeException("height");
             }
 

Sample/Fractal.cs
@@ -59,7 +59,7 @@ namespace Sample
         public int Iterations
         {
             get { return iterations; }
-            set { GL.Uniform1(GL.GetUniformLocation(shHandle, "iters"), (uint)(iterations = value)); }
+            set { GL.Uniform1(GL.GetUniformLocation(shHandle, "iters"), iterations = value); }
         }
 
         private bool hardcodePolynomial;

Sample/Program.cs
@@ -298,7 +298,11 @@ namespace Sample
 
             KeyPress += (sender, e) => context.HandleKeyPress(e.KeyChar);
             Resize += (sender, e) => context.HandleResize(ClientSize);
-            KeyDown += (sender, e) => HandleKeyPress(context, e);
+            KeyDown += (sender, e) => {
+                if (!HandleKeyPress(context, e) && (e.Key == Key.Escape)) {
+                    Close(); // Close program on Esc when appropriate
+                }
+            };
 
             Mouse.WheelChanged += (sender, e) => fractal.ZoomIn(e.DeltaPrecise);
             Mouse.Move += (sender, e) => context.HandleMouseMove(e.Position);

commit 8a09e64775b14978b00f972929c5b5f780410941

Fixed regression from 3f8df7a5373caac0816f6bb21ad5fd64af677ab9 (uninitialized shader variable)

Sample/Shader.cs
@@ -146,7 +146,7 @@ namespace Sample
             str.AppendLine();
             str.AppendLine("vec4 iterate(vec2 z)");
             str.AppendLine("{");
-            str.AppendLine("    float speed;");
+            str.AppendLine("    float speed = 0;");
             str.AppendLine("    int t;");
             str.AppendLine();
             str.AppendLine("    for (t = int(0); t < iters; ++t)");
@@ -207,7 +207,7 @@ namespace Sample
             str.AppendLine("vec3 shade(vec2 z)");
             str.AppendLine("{");
             str.AppendLine("    vec4 r = iterate(z);");
-            str.AppendLine("    return colorize(z, r.xy, pow(r.z, intensity), r.w * r.z / iters);");
+            str.AppendLine("    return colorize(z, r.xy, pow(r.z, intensity), r.w * intensity / iters);");
             str.AppendLine("}");
 
             return str.ToString();